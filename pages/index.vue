ec967439183e6306ba1c483683cf954cf3a18f79
marcogmurciano <marcogmurciano@gmail.com>
2025-03-04 14:51:31 +0100

    cliente satisfecho

--- pages/index.vue
+++ pages/index.vue
@@ -2,14 +2,24 @@
     <title>I am what i am // invitación</title>
     <div class="flex flex-col text-center p-5 sofia bg-[#24032B]">
         <div class="md:w-[50%] md:pt-[14%] pt-20 w-[90%] mx-auto  gap-[2rem] flex flex-col">
-            <h1 ref="neonText" class="neon-pink font-bold">I AM WHAT I AM</h1>
+            <h1 ref="neonText" class="neon-pink slab whitespace-nowrap">I AM WHAT I AM</h1>
             <div class="flex flex-col gap-[1.1rem] text-white">
                 <p class="text-[#EA52EA] uppercase">
-                    Sábado 5 de Abril en
+                    Sabado 5 de Abril en
                     Sissy Bag <br> C. de Pelayo, 42, Centro, 28004 Madrid
                     <br> 21:00
                 </p>
-                <p class="">
+                <iframe class="mt-8" width="100%" height="315"
+                    src="https://www.youtube.com/embed/rs-2T6u1E2M?si=oBvzK2fYC6G3LfVI" title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <div class="flex mx-auto mt-20 mb-14">
+                    <PhArrowFatDown ref="neonArr" class="arrow-pink" :size="128" />
+                </div>
+
+   
+                <p class="mt-8">
                     Estás invitade a la fiesta de la expresión y la purísima identidad
                     Ven con tu look más único y personal, no quiero que seas extra si no es lo
                     tuyo, quiero que seas TÚ.<br>
@@ -23,29 +33,9 @@
                     Es una fiesta de libertad, NO ES UNA COMPETICIÓN ¡¡¡Se exige disfrute,
                     respeto y amor al resto de artistas y mucho coño!!!
                 </p>
-                <!-- Estás invitade a la fiesta de la expresión y la purísima identidad
-                    Ven con tu look más único y personal, no quiero que seas extra si no es lo
-                    tuyo, quiero que seas TÚ <br>
-                    Todo lo excéntrico, extravagante, raro, maniático, estrafalario, curioso,
-                    atípico y extraordinario es bienvenido. <br>
-                    Tienes toda la libertad de expresarte como quieras, en una performance, una
-                    canción, un cuadro, una escultura... házmelo saber para poder organizar el
-                    orden. <br>
-                    Por ende, habrá todo tipo de shows del resto de artistas y música vibrante
-                    que te hará querer darlo todo. <br>
-                    Es una fiesta de libertad, NO ES UNA COMPETICIÓN ¡¡¡se exige disfrute,
-                    respeto y amor al resto de artistas y mucho coño!!! -->
-                <h2 ref="neonText2" class="neon-pink mt-6">
+                <h2 ref="neonText2" class="neon-pink mt-6 mb-8 slab">
                     A DARLO TODO P00TAS
                 </h2>
-                <div class="flex mx-auto mt-32">
-                    <PhArrowFatDown ref="neonArr" class="arrow-pink" :size="128" />
-                </div>
-                <iframe class="mt-32" width="100%" height="315"
-                    src="https://www.youtube.com/embed/rs-2T6u1E2M?si=oBvzK2fYC6G3LfVI" title="YouTube video player"
-                    frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </div>
         </div>
     </div>
@@ -94,4 +84,32 @@ onMounted(() => {
     .arrow-pink
       color: #F0D9F0
       filter: drop-shadow(0 0 2px #ff77ff) drop-shadow(0 0 7px #ff77ff) drop-shadow(0 0 15px #ff77ff) drop-shadow(0 0 30px #ff77ff)
+    p
+        font-size: 20px
+        font-weight: 200
+
+
+h2
+    font-size: 40px
+
+h1
+    font-size: 55px
+    line-height: 40px
+    font-weight: 400
+
+@media (max-width: 500px) 
+    *
+        font-size: 10px
+
+    h1 
+        font-size: 45px
+    
+    h2
+        font-size: 30px
+    
+    p
+        font-size: 17px
+        font-weight: 300
+    
+
 </style>
